558c9a47422782d25ebf4fe0b5a7cb59006dfa45
mohdgauri7 <79560111+mohdgauri7@users.noreply.github.com>
2026-07-06 23:27:48 +0800

    five REST endPoint are created.

--- src/main/java/com/mamg/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/mamg/journalApp/controller/JournalEntryController.java
@@ -1,46 +1,57 @@
 package com.mamg.journalApp.controller;
 
 import com.mamg.journalApp.entity.JournalEntry;
+import com.mamg.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("journal")
 public class JournalEntryController {
 
-
-    Map<Long, JournalEntry> journalEntries = new HashMap<>();
-
+    @Autowired
+    private JournalEntryService journalEntryService;
 
     @GetMapping
     public List<JournalEntry> getAll() {
-        return new ArrayList<>(journalEntries.values());
+        return journalEntryService.getAll();
     }
 
     @PostMapping
     public boolean createEntry(@RequestBody JournalEntry journalEntry) {
-        journalEntries.put(journalEntry.getId(), journalEntry);
+        journalEntry.setDate(LocalDateTime.now());
+        journalEntryService.saveEntry(journalEntry);
         return true;
     }
 
     @GetMapping("/id/{myId}")
-    public JournalEntry getEntityById(@PathVariable Long myId) {
-        return journalEntries.get(myId);
+    public JournalEntry getEntityById(@PathVariable ObjectId myId) {
+        journalEntryService.findById(myId).orElse(null);
+        return null;
     }
 
     @DeleteMapping("/id/{myId}")
-    public JournalEntry deleteEntityById(@PathVariable Long myId) {
-        return journalEntries.remove(myId);
+    public boolean deleteEntityById(@PathVariable ObjectId myId) {
+        journalEntryService.deleteById(myId);
+        return true;
     }
 
     @PutMapping("/id/{myId}")
-    public JournalEntry updateEntityById(@PathVariable Long myId, @RequestBody JournalEntry journalEntry) {
-        return journalEntries.put(myId, journalEntry);
+    public JournalEntry updateEntityById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntity) {
+        JournalEntry oldEntity = journalEntryService.findById(myId).orElse(null);
+        if (oldEntity != null) {
+            oldEntity.setTitle(newEntity.getTitle() != null && newEntity.getTitle().equals("") ? oldEntity.getTitle() : newEntity.getTitle());
+            oldEntity.setContent(newEntity.getContent() != null && newEntity.getContent().equals("") ? oldEntity.getContent() : newEntity.getContent());
+            journalEntryService.saveEntry(oldEntity);
+        }
+        return oldEntity;
     }
 
 
 }
+
+//Continue From Here: https://www.youtube.com/watch?v=tWBhE1Cn8D0&list=PLA3GkZPtsafacdBLdd3p1DyRd5FGfr3Ue&index=14
---
+++ src/main/java/com/mamg/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,10 @@
+package com.mamg.journalApp.repository;
+
+import com.mamg.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+
+
+}
